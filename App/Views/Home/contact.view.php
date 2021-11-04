4a1da2e49c14afceef30c3e6a8cf9e9f37141664
Patrik Hrkút <hrkut@kst.uniza.sk>
2021-11-04 15:24:37 +0100

    Update to Bootstrap 5

--- App/Views/Home/contact.view.php
+++ App/Views/Home/contact.view.php
@@ -1,19 +1,18 @@
 <?php /** @var Array $data */ ?>
-<div class="container">
-    <h1 class="mt-5">Kde nás najdete?</h1>
-    <p class="lead">Wiki, World Wide Web (WWW) site that can be modified or contributed to by users. Wikis can be dated to 1995, when American computer programmer Ward Cunningham created a new collaborative technology for organizing information on Web sites. Using a Hawaiian term meaning “quick,” he called this new software WikiWikiWeb, attracted by its alliteration and also by its matching abbreviation (WWW).</p>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <h1>Fakulta riadenia a informatiky</h1>
+            <strong>Adresa</strong>: Univerzitná 8215/1, 010 26 Žilina<br>
+            <strong>Tel. číslo</strong>: +421/41 513 4061<br>
 
-    <p>
-        Wikis were inspired in part by Apple’s HyperCard program, which allowed users to create virtual “card stacks” of information with a host of connections, or links, among the various cards. HyperCard in turn drew upon an idea suggested by Vannevar Bush in his 1945 Atlantic Monthly article “As We May Think.” There Bush envisioned the memex, a machine that would allow readers to annotate and create links between articles and books recorded on microfilm. HyperCard’s “stacks” implemented a version of Bush’s vision, but the program relied upon the user to create both the text and the links. For example, one might take a musical score of a symphony and annotate different sections with different cards linked together.
-    </p>
+            <strong>GPS</strong>: 49°12'6,4"N 18°45'42,6"E
+        </div>
+    </div>
 </div>
-
-<div class="container">
-
-<?php
-$lat = $data['lat'];//"33.80766";
-$log =  $data['lng']; //"-33.76714";
-
-?>
-<iframe id="map-canvas" class="map_part" width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=<?php echo $lat.",".$log; ?>&amp;t=&amp;z=3&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+<div class="row mt-3">
+    <div class="col">
+        <iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                src="https://www.openstreetmap.org/export/embed.html?bbox=18.747396469116214%2C49.193792384417996%2C18.776578903198246%2C49.210336337994846&amp;layer=mapnik&amp;marker=49.202065053033984%2C18.761987686157227"></iframe>
+    </div>
 </div>
--- App/Views/root.layout.view.php
+++ App/Views/root.layout.view.php
@@ -1,23 +1,39 @@
-<html>
-    <head>
-        <title>Super úžasná stránka</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="public/css.css">
-    </head>
-    <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal">Super úžasná stránka</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark active" href="?c=home">Domov</a>
-            <a class="p-2 text-dark" href="?c=home&a=contact">Kontakt</a>
-        </nav>
-
+<html lang="sk">
+<head>
+    <title>FRI-MVC FW</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="public/css.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-end">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">FRI-MVC FW </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="?c=home">Domov</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?c=home&a=contact">Kontakt</a>
+                </li>
+            </ul>
+        </div>
     </div>
-    <div class="web-content">
-        <?= $contentHTML ?>
+</nav>
+<div class="conatiner-fluid">
+    <div class="row mt-2">
+        <div class="col">
+                <?= $contentHTML ?>
+        </div>
     </div>
-    </body>
+</div>
+</body>
 </html>
+
