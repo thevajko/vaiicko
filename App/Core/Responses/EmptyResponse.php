ab4b336beb98e8eaa0770969ffb1958d929b09b1
Michal Ďuračík <Michal.Duracik@fri.uniza.sk>
2023-10-23 14:01:13 +0200

    Added EmptyResponse

---
+++ App/Core/Responses/EmptyResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Core\Responses;
+
+/**
+ * Class EmptyResponse
+ * Empty response with no content
+ * @package App\Core\Responses
+ */
+class EmptyResponse extends Response
+{
+    public function __construct()
+    {
+        $this->setStatusCode(201);
+    }
+
+
+    protected function generate(): void
+    {
+    }
+}
