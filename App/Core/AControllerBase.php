a8ecb140d68b160a6be66a019b139c444df49072
Michal Ďuračík <Michal.Duracik@fri.uniza.sk>
2023-10-23 14:04:21 +0200

    Controller helper methods should be protected.

--- App/Core/AControllerBase.php
+++ App/Core/AControllerBase.php
@@ -25,7 +25,7 @@ abstract class AControllerBase
      * Returns controller name (without Controller prefix)
      * @return string
      */
-    public function getName()  : string
+    public function getName() : string
     {
         return str_replace("Controller", "", $this->getClassName());
     }
@@ -34,7 +34,7 @@ abstract class AControllerBase
      * Return full class name
      * @return string
      */
-    public function getClassName()
+    public function getClassName() : string
     {
         $arr = explode("\\", get_class($this));
         return end($arr);
@@ -70,7 +70,7 @@ abstract class AControllerBase
      * @param $data
      * @return JsonResponse
      */
-    public function json($data) : JsonResponse
+    protected function json($data) : JsonResponse
     {
         return new JsonResponse($data);
     }
@@ -80,7 +80,7 @@ abstract class AControllerBase
      * @param string $redirectUrl
      * @return RedirectResponse
      */
-    public function redirect(string $redirectUrl) : RedirectResponse
+    protected function redirect(string $redirectUrl) : RedirectResponse
     {
         return new RedirectResponse($redirectUrl);
     }
@@ -89,7 +89,7 @@ abstract class AControllerBase
      * Helper method for request
      * @return Request
      */
-    public function request() : Request
+    protected function request() : Request
     {
         return $this->app->getRequest();
     }
