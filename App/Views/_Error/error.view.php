eb9036a16357788e2f93ff0f42093a85d40cfb0e
Patrik Hrkút <Patrik.Hrkut@fri.uniza.sk>
2023-11-11 21:05:43 +0100

    Fix in error.view.php. There are no details about user exception.

--- App/Views/_Error/error.view.php
+++ App/Views/_Error/error.view.php
@@ -8,6 +8,13 @@ $exception = $data["exception"];
 
 <h1><?= $exception->getCode() . " - " . $exception->getMessage() ?></h1>
 
+<?php if ($data["showDetail"] && $exception->getCode() != 500) { ?>
+    <?= get_class($exception) ?>: <strong><?= $exception->getMessage() ?></strong>
+    in file <strong><?= $exception->getFile() ?></strong>
+    at line <strong><?= $exception->getLine() ?></strong>
+    <pre>Stack trace:<br><?= $exception->getTraceAsString() ?></pre>
+<?php } ?>
+
 <?php
 while ($data["showDetail"] && $exception->getPrevious() != null) { ?>
     <?= get_class($exception->getPrevious()) ?>: <strong><?= $exception->getPrevious()->getMessage() ?></strong>
