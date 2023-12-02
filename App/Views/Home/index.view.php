354c1b31aac176974bfb1250f4dfd18c9592c9fd
zxkredo <lukasmaga3@gmail.com>
2023-12-02 11:00:28 +0100

    inserted from first checkpoint

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -1,30 +1,43 @@
-<div class="container-fluid">
+<div class="container">
+    <!-- Video -->
     <div class="row">
-        <div class="col mt-5">
-            <div class="text-center">
-                <h2>Vaííčko MVC FW</h2>
-                <img src="public/images/vaiicko_logo.png">
-                <p>
-                    Gratulujeme vám, úspešne sa vám podarilo nainštalovať a spustiť framework
-                    <strong>Vaííčko</strong> <?= \App\Config\Configuration::FW_VERSION ?>!<br>
-                    Dúfame, že sa Vám pomocou tohto frameworku vytvoríte nejakú skvelú aplikáciu.<br>
-                </p>
-                <p>
-                    Tento jednoduchý framework bol vytvorený pre lepšie pochopenie architektúry MVC.<br>
-                    Je určený pre študentov predmetu <em>vývoj aplikácií pre internet a intranet</em>, ale nielen im.
-                </p>
-            </div>
+        <div class="promotional_video embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/DMHxEmNdOMg?rel=0" ></iframe>
         </div>
     </div>
-    <div class="row mt-3">
-        <div class="col text-center">
-            <h4>Autori</h4>
-            <div>
-                <a href="mailto:Patrik.Hrkut@fri.uniza.sk">doc. Ing. Patrik Hrkút, PhD.</a><br>
-                <a href="mailto:Michal.Duracik@fri.uniza.sk">Ing. Michal Ďuračík, PhD.</a><br>
-                <a href="mailto:Matej.Mesko@fri.uniza.sk">Ing. Matej Meško, PhD.</a><br><br>
-                &copy; 2020-<?= date('Y') ?> Žilinská univerzita v Žiline, Fakulta riadenia a informatiky, Katedra
-                softvérových technológií
+    <!-- Main content -->
+    <div class="row">
+        <div class="logos col-3 d-none d-sm-block">
+            <img class="img-fluid row" src="../images/logos/ITRA_Logo.png" alt="Logo spoločnosti ITRA">
+            <img class="img-fluid row" src="../images/logos/SlovakUltraTrail_Logo.png" alt="Logo spoločnosti Sovak Ultra Trail">
+        </div>
+        <div class="col">
+            <div class="header row justify-content-md-center">
+                <h1>Vitajte na našom webe</h1>
+            </div>
+            <div class="description row">
+                <p>
+                    Malofatranská stovka sú amatérske preteky v pohorí Malej Fatry v kategórii ultra trail (horský ultramaratón).
+                    MF100 je registrovaným členom ITRA a každoročne sa uchádza o štatút kvalifikačného preteku pre podujatia združené v <a href="https://itra.run/" target="_blank">ITRA</a>,
+                    napr. jeden z najznámejších horských ultramaratónov v Európe <a href="https://montblanc.utmb.world/" target="_blank">Ultra-Trail du Mont-Blanc®</a>.
+                </p>
+                <p>
+                    Predpokladom úspešného dokončenia niektorej z trás MF100 je výborná fyzická kondícia a dostatočné skúsenosti s horskými pretekmi.
+                </p>
+                <p>
+                    Trasa prechádza celým pohorím Malá Fatra (Krivánska Fatra a Lúčanská Fatra) s výraznými stúpaniami a klesaniami a viacerými exponovanými miestami.
+                </p>
+                <p>
+                    Organizátorom MF100 je <a href="https://www.slovakultratrail.sk/" target="_blank">SLOVAK ULTRA TRAIL</a>, o.z. v spolupráci s dobrovoľníkmi a partnermi podujatia.
+                </p>
+            </div>
+            <div class="links row">
+                <p class="col">
+                    <a href="https://www.youtube.com/watch?v=DMHxEmNdOMg" target="_blank">MALOFATRANSKÁ STOVKA - official trailer</a>
+                </p>
+                <p class="col">
+                    <a href="https://www.youtube.com/watch?v=2Z3XCmTtPs4" target="_blank">MALOFATRANSKÁ STOVKA 2023 - film</a>
+                </p>
             </div>
         </div>
     </div>
