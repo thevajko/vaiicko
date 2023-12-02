8ab24287e422af70d46b91648c2b38beb7bcf6d0
zxkredo <lukasmaga3@gmail.com>
2023-12-02 17:02:56 +0100

    form validation

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -4,8 +4,12 @@ namespace App\Controllers;
 
 use App\Config\Configuration;
 use App\Core\AControllerBase;
+use App\Core\HTTPException;
+use App\Core\LinkGenerator;
 use App\Core\Responses\Response;
 use App\Core\Responses\ViewResponse;
+use DateTime;
+use Exception;
 
 /**
  * Class AuthController
@@ -21,6 +25,11 @@ class AuthController extends AControllerBase
     public function index(): Response
     {
         return $this->redirect(Configuration::LOGIN_URL);
+    }
+
+    public function registracia() : Response
+    {
+        return $this->html();
     }
 
     /**
@@ -51,4 +60,89 @@ class AuthController extends AControllerBase
         $this->app->getAuth()->logout();
         return $this->html();
     }
+
+    /**
+     * @throws HTTPException
+     */
+    public function register() : Response
+    {
+        $formData = $this->app->getRequest()->getPost();
+        if ($this->checkForm($formData)
+        ) {
+            $name = $formData['name'];
+            $surname = $formData['surname'];
+            $gender = $formData['gender'];
+            $birthDate = $formData['birthDate'];
+            $street = $formData['street'];
+            $city = $formData['city'];
+            $postalCode = $formData['postalCode'];
+            $email = $formData['email'];
+            $password = $formData['password'];
+        }
+        else {
+            throw new HTTPException(400, "Missing parameter");
+        }
+
+        return $this->html(LinkGenerator::url("auth.login"));
+    }
+
+    private function checkForm($formData) : bool
+    {
+        if (!isset($formData['submit'])
+            || !isset($formData['name'])
+            || !isset($formData['surname'])
+            || !isset($formData['gender'])
+            || !isset($formData['birthDate'])
+            || !isset($formData['street'])
+            || !isset($formData['city'])
+            || !isset($formData['postalCode'])
+            || !isset($formData['email'])
+            || !isset($formData['password'])
+        )
+        {
+            return false;
+        }
+
+        if (empty($formData['submit'])
+            || empty($formData['name'])
+            || empty($formData['surname'])
+            || empty($formData['gender'])
+            || empty($formData['birthDate'])
+            || empty($formData['street'])
+            || empty($formData['city'])
+            || empty($formData['postalCode'])
+            || empty($formData['email'])
+            || empty($formData['password'])
+        )
+        {
+            return false;
+        }
+
+        $gender = $formData['gender'];
+        if ($gender != "female" || $gender != "male" || $gender != "other")
+        {
+            return false;
+        }
+
+        $birthDate = DateTime::createFromFormat('Y-m-d', $formData['birthDate']);
+        if (!$birthDate || $birthDate->format('Y-m-d') !== $formData['birthDate'])
+        {
+            return false;
+        }
+
+        $postalCode = $formData['postalCode'];
+        if (!preg_match('/\d{3} ?\d{2}$/', $postalCode))
+        {
+            return false;
+        }
+
+        $email = $formData['email'];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
+
