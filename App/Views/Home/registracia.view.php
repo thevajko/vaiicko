96ddb829df827466c29ee914ac61cf32b4496a33
zxkredo <lukasmaga3@gmail.com>
2023-12-02 13:29:51 +0100

    browser form validation

--- App/Views/Home/registracia.view.php
+++ App/Views/Home/registracia.view.php
@@ -1,3 +1,4 @@
+<script src="public/js/formCheck.js"></script>
 <section class="h-100 bg-dark">
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
@@ -10,41 +11,41 @@
                                  style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;" />
                         </div>
                         <div class="col-xl-6">
-                            <form class="row g-3 needs-validation" novalidate>
+                            <form class="row g-3 needs-validation">
                                 <div class="card-body p-md-5 text-black">
                                     <h3 class="mb-5 text-uppercase">Registácia bežca</h3>
 
                                     <div class="row">
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <input type="text" id="meno" class="form-control form-control-lg" />
+                                                <input type="text" id="meno" class="form-control form-control-lg" required/>
                                                 <label class="form-label" for="meno">Meno</label>
                                             </div>
                                         </div>
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <input type="text" id="priezvisko" class="form-control form-control-lg" />
+                                                <input type="text" id="priezvisko" class="form-control form-control-lg" required/>
                                                 <label class="form-label" for="priezvisko">Priezvisko</label>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div class="form-outline mb-4">
-                                        <input type="text" id="ulica" class="form-control form-control-lg" />
+                                        <input type="text" id="ulica" class="form-control form-control-lg" required/>
                                         <label class="form-label" for="ulica">Ulica</label>
                                     </div>
 
                                     <div class="row">
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <input type="text" id="mesto" class="form-control form-control-lg" />
+                                                <input type="text" id="mesto" class="form-control form-control-lg" required/>
                                                 <label class="form-label" for="mesto">Mesto</label>
                                             </div>
                                         </div>
 
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <input type="text" id="psc" class="form-control form-control-lg" />
+                                                <input type="text" id="psc" class="form-control form-control-lg" required/>
                                                 <label class="form-label" for="psc">PSČ</label>
                                             </div>
                                         </div>
@@ -56,27 +57,27 @@
 
                                         <div class="form-check form-check-inline mb-0 me-4">
                                             <input class="form-check-input" type="radio" name="inlineRadioOptions" id="zena"
-                                                   value="option1" />
+                                                   value="option1" required/>
                                             <label class="form-check-label" for="zena">Žena</label>
                                         </div>
 
                                         <div class="form-check form-check-inline mb-0 me-4">
                                             <input class="form-check-input" type="radio" name="inlineRadioOptions" id="muz"
-                                                   value="option2" />
+                                                   value="option2" required/>
                                             <label class="form-check-label" for="muz">Muž</label>
                                         </div>
 
                                         <div class="form-check form-check-inline mb-0">
                                             <input class="form-check-input" type="radio" name="inlineRadioOptions" id="ine"
-                                                   value="option3" />
+                                                   value="option3" required/>
                                             <label class="form-check-label" for="ine">Iné</label>
                                         </div>
 
                                     </div>
 
                                     <div class="form-outline mb-4">
-                                        <input type="text" id="form3Example9" class="form-control form-control-lg" />
-                                        <label class="form-label" for="form3Example9">DOB</label>
+                                        <input type="text" id="email" class="form-control form-control-lg" required/>
+                                        <label class="form-label" for="email">Email</label>
                                     </div>
 
                                     <div class="d-flex justify-content-end pt-3">
