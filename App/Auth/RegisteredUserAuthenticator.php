ca148442b71cff4c774b9ae987b504511d74b322
zxkredo <lukasmaga3@gmail.com>
2023-12-02 19:43:51 +0100

    Authenticator for the part of website where any registered user can log in

---
+++ App/Auth/RegisteredUserAuthenticator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Auth;
+
+use App\Auth\DummyAuthenticator;
+use App\Models\Login;
+
+class RegisteredUserAuthenticator extends DummyAuthenticator
+{
+    /**
+     * Verify, if the user is in DB and has his password is correct
+     * @param $login
+     * @param $password
+     * @return bool
+     * @throws \Exception
+     */
+    public function login($login, $password): bool
+    {
+        $loggedUser = Login::getByLogin($login);
+        if (is_null($loggedUser))
+        {
+            return false;
+        }
+        else if ($login == $loggedUser && password_verify($password, $loggedUser->getPassword())) {
+            $_SESSION['user'] = $loggedUser->getLogin();
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
