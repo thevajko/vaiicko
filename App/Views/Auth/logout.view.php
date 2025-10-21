ca016354ea1b29a1a96cf6d6612a2c50c40c353c
Patrik Hrkút <Patrik.Hrkut@fri.uniza.sk>
2025-10-21 21:15:17 +0200

    Update logout message for clarity

--- App/Views/Auth/logout.view.php
+++ App/Views/Auth/logout.view.php
@@ -6,7 +6,7 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-9 col-md-7 col-lg-5">
-            You have logged out. <br>
+            You have been logged out. <br>
             Again <a href="<?= App\Configuration::LOGIN_URL ?>">log in</a> or return <a
                     href="<?= $link->url("home.index") ?>">back</a> to the home page?
         </div>
