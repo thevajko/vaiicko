79cafd72eb6018132174f0dd410d6831bc69030a
Patrik Hrkút <Patrik.Hrkut@fri.uniza.sk>
2025-10-21 20:07:12 +0200

    Refactor layout views to use LinkGenerator for asset URLs and update Bootstrap version

--- App/Views/Layouts/auth.layout.view.php
+++ App/Views/Layouts/auth.layout.view.php
@@ -2,6 +2,7 @@
 
 /** @var string $contentHTML */
 /** @var \Framework\Core\IAuthenticator $auth */
+/** @var \Framework\Helpers\LinkGenerator $link */
 ?>
 
 <!DOCTYPE html>
@@ -9,24 +10,18 @@
 <head>
     <title><?= App\Configuration::APP_NAME ?></title>
     <!-- Favicons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
-    <link rel="manifest" href="/favicons/site.webmanifest">
-    <link rel="shortcut icon" href="/favicons/favicon.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= $link->asset('favicons/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= $link->asset('favicons/favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= $link->asset('favicons/favicon-16x16.png') ?>">
+    <link rel="manifest" href="<?= $link->asset('favicons/site.webmanifest') ?>">
+    <link rel="shortcut icon" href="<?= $link->asset('favicons/favicon.ico') ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
             crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-            integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"
-            integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk"
-            crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/styl.css">
-    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="<?= $link->asset('css/styl.css') ?>">
+    <script src="<?= $link->asset('js/script.js') ?>"></script>
 </head>
 <body>
 <div class="container-fluid mt-3">
--- App/Views/Layouts/root.layout.view.php
+++ App/Views/Layouts/root.layout.view.php
@@ -2,42 +2,42 @@
 
 /** @var string $contentHTML */
 /** @var \Framework\Core\IAuthenticator $auth */
-/** @var \Framework\Core\LinkGenerator $link */
+/** @var \Framework\Helpers\LinkGenerator $link */
 ?>
 <!DOCTYPE html>
 <html lang="sk">
 <head>
     <title><?= App\Configuration::APP_NAME ?></title>
     <!-- Favicons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
-    <link rel="manifest" href="/favicons/site.webmanifest">
-    <link rel="shortcut icon" href="/favicons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= $link->asset('favicons/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= $link->asset('favicons/favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= $link->asset('favicons/favicon-16x16.png') ?>">
+    <link rel="manifest" href="<?= $link->asset('favicons/site.webmanifest') ?>">
+    <link rel="shortcut icon" href="<?= $link->asset('favicons/favicon.ico') ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
             crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/styl.css">
-    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="<?= $link->asset('css/styl.css') ?>">
+    <script src="<?= $link->asset('js/script.js') ?>"></script>
 </head>
 <body>
 <nav class="navbar navbar-expand-sm bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="<?= $link->url("home.index") ?>">
-            <img src="images/vaiicko_logo.png" title="<?= App\Configuration::APP_NAME ?>" alt="Framework Logo">
+        <a class="navbar-brand" href="<?= $link->url('home.index') ?>">
+            <img src="<?= $link->asset('images/vaiicko_logo.png') ?>" title="<?= App\Configuration::APP_NAME ?>" alt="Framework Logo">
         </a>
         <ul class="navbar-nav me-auto">
             <li class="nav-item">
-                <a class="nav-link" href="<?= $link->url("home.contact") ?>">Contact</a>
+                <a class="nav-link" href="<?= $link->url('home.contact') ?>">Contact</a>
             </li>
         </ul>
         <?php if ($auth?->isLogged()) { ?>
             <span class="navbar-text">Logged in user: <b><?= $auth?->getLoggedUserName() ?></b></span>
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="<?= $link->url("auth.logout") ?>">Log out</a>
+                    <a class="nav-link" href="<?= $link->url('auth.logout') ?>">Log out</a>
                 </li>
             </ul>
         <?php } else { ?>
