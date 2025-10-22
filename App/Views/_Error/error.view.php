e0e2288c52c48a6503325a5ff11380e9aca40310
Patrik Hrkút <hrkut@kst.uniza.sk>
2025-10-22 18:50:17 +0200

    Refactor error view to use explicit variable for detail display

--- App/Views/_Error/error.view.php
+++ App/Views/_Error/error.view.php
@@ -1,17 +1,17 @@
 <?php
 
-/** @var array $data */
 /** @var \Framework\Http\HttpException $exception */
+/** @var bool $showDetail */
 /** @var \Framework\Support\View $view */
 
 $view->layout(null);
-$exception = $data["exception"];
+
 ?>
 
 <h1><?= $exception->getCode() . " - " . $exception->getMessage() ?></h1>
 
 <?php
-if ($data["showDetail"] && $exception->getCode() != 500) :
+if ($showDetail && $exception->getCode() != 500) :
     ?>
     <?= get_class($exception) ?>: <strong><?= $exception->getMessage() ?></strong>
     in file <strong><?= $exception->getFile() ?></strong>
@@ -20,7 +20,7 @@ if ($data["showDetail"] && $exception->getCode() != 500) :
 <?php endif; ?>
 
 <?php
-while ($data["showDetail"] && $exception->getPrevious() != null) { ?>
+while ($showDetail && $exception->getPrevious() != null) { ?>
     <?= get_class($exception->getPrevious()) ?>: <strong><?= $exception->getPrevious()->getMessage() ?></strong>
     in file <strong><?= $exception->getPrevious()->getFile() ?></strong>
     at line <strong><?= $exception->getPrevious()->getLine() ?></strong>
